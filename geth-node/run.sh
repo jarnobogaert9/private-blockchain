8aeba039e0037cf4e8c143eb75fd11db31852257
Jarno Bogaert <jarnobogaert9@gmail.com>
2021-02-25 15:57:23 +0100

    chainId updated

--- geth-node/run.sh
+++ geth-node/run.sh
@@ -4,4 +4,4 @@ echo "========================
        Init done. 
 ========================"
 
-geth --networkid 5333 --datadir ./data/$1 --nodiscover --http --http.port "8545" --port "30303" --http.corsdomain "*" --http.addr "0.0.0.0" --http.api="eth,net,personal,web3,debug,admin,miner" --ipcdisable
+geth --networkid 7333 --datadir ./data/$1 --nodiscover --http --http.port "8545" --port "30303" --http.corsdomain "*" --http.addr "0.0.0.0" --http.api="eth,net,personal,web3,debug,admin,miner" --ipcdisable

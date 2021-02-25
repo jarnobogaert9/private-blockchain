24e8f5e3cec35e5601ff08cdd675ec496cb644f4
Jarno Bogaert <jarnobogaert9@gmail.com>
2021-02-25 14:54:46 +0100

    Refactored folder structure

---
+++ geth-node/run.sh
@@ -0,0 +1,7 @@
+geth init --datadir ./data/$1 ./data/$2.json
+
+echo "======================== 
+       Init done. 
+========================"
+
+geth --networkid 5333 --datadir ./data/$1 --nodiscover --http --http.port "8545" --port "30303" --http.corsdomain "*" --http.addr "0.0.0.0" --http.api="eth,net,personal,web3,debug,admin,miner" --ipcdisable

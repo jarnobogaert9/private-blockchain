ba0070f187477aeaf8607986d5873fa9dbaa8f08
Jarno Bogaert <jarnobogaert9@gmail.com>
2021-02-26 12:46:00 +0100

    Updated chainID, save data after each block

--- geth-node/run.sh
+++ geth-node/run.sh
@@ -1,7 +1,16 @@
+echo "Datafolder: ./$1/"
+echo "Config file: ./$2.json"
+echo "http port: $3"
+echo "port: $4"
+echo "network/chain ID: $5"
+
 geth init --datadir ./data/$1 ./data/$2.json
 
 echo "======================== 
        Init done. 
 ========================"
+echo "======================== 
+    Starting node... 
+========================"
 
-geth --networkid 7333 --datadir ./data/$1 --nodiscover --http --http.port "8545" --port "30303" --http.corsdomain "*" --http.addr "0.0.0.0" --http.api="eth,net,personal,web3,debug,admin,miner" --ipcdisable
+geth --networkid $5 --datadir ./data/$1 --nodiscover --http --http.port $3 --port $4 --http.corsdomain "*" --http.addr "0.0.0.0" --http.api="eth,net,personal,web3,debug,admin,miner" --ipcdisable --gcmode archive
